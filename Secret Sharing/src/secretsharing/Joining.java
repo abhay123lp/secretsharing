bebca266d3d20dfa95e6899be9411103bb2a2294
vitaliaa <vitaliaa@5004abfe-0aec-8945-64b4-278708e008a8>
2010-03-24 13:57:30 +0000

    Created project's structure

---
+++ Secret Sharing/src/secretsharing/Joining.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package secretsharing;
+
+/**
+ *
+ * @author vitaa
+ */
+public class Joining {
+
+}
