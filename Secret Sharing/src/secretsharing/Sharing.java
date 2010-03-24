974badef7916b52e6f1cfc8f01814202650b8533
vitaliaa <vitaliaa@5004abfe-0aec-8945-64b4-278708e008a8>
2010-03-24 14:46:10 +0000

    Added Embedding function, realised Lagrange's method to sharing secret.

--- Secret Sharing/src/secretsharing/Sharing.java
+++ Secret Sharing/src/secretsharing/Sharing.java
@@ -5,10 +5,50 @@
 
 package secretsharing;
 
-/**
- *
- * @author vitaa
- */
+import java.math.BigInteger;
+import steganographyException.*;
+
 public class Sharing {
 
+    public Sharing(BigInteger message, BigInteger prime) {
+        this.message = message;
+        this.prime = prime;
+    }
+
+    void generatePolynom(int power) {
+        /* Generate Polynom modulo prime */
+        coefficients = new BigInteger[power + 1];
+        for (int i = 0; i <= power; i++) {
+            coefficients[i] = prime.nextProbablePrime();
+            coefficients[i] = coefficients[i].mod(prime);
+        }
+    }
+
+    BigInteger getPolynomValue(BigInteger arg) {
+        BigInteger value = BigInteger.valueOf(0);
+        value = value.add(message);
+        for (int i = 0; i < coefficients.length; i++) {
+            BigInteger k = arg.pow(i + 1);
+            k = k.multiply(coefficients[i]);
+            value = value.add(k);
+        }
+        value = value.mod(prime);
+        return value;
+    }
+
+    public BigInteger[] getShares(BigInteger[] args, int k, int n) throws BadQuantityArgumentsException {
+        /* Create (k,n)threshold scheme */
+        if (args.length != n) {
+            throw new BadQuantityArgumentsException();
+        }
+        BigInteger[] shares = new BigInteger[n];
+        generatePolynom(k - 1);
+        for (int i = 0; i < shares.length; i++) {
+            shares[i] = getPolynomValue(args[i]);
+        }
+        return shares;
+    }
+    private BigInteger message;
+    private BigInteger prime;
+    private BigInteger[] coefficients;
 }
